eca0beb92ddb248edb9d137e8f2d97e7ca82ef8c
Shubham Dokania <skd.1810@gmail.com>
2015-04-04 02:43:24 +0530

    book scraper

--- bookd/isbn_bookd.py
+++ bookd/isbn_bookd.py
@@ -9,11 +9,12 @@ def get_data(isbn):
         book_name = soup.find('h1', {'class': 'bookMainTitle'})
         rating = soup.find('div', {'class': ' col-sm-4 col-xs-12 userAggregatedRatingBox ratingPositive'})
         image = soup.find('img', {'class': 'bookMainImage'})
-        summary = soup.findAll('em')
+        summary = soup.findAll('em')[0].string
 
         print "Book : " + str(book_name.string.split("\n")[1].split("    ")[5])
         print "ratings : " + str(rating.string.split("\n")[1].split(" ")[22])
         print "Image : " + str(image['src'])
+        print summary
 
     except urllib2.HTTPError:
         pass
@@ -28,16 +29,31 @@ def google_books(isbn):
 
     try:
         book_data = data['items'][0]['volumeInfo']
-        name = book_data['name']
+        name = book_data['title']
+        authors = ""
+        for i in book_data['authors']:
+            if len(book_data['authors']) is not 1:
+                authors = authors.join(", " + str(i))
+            else:
+                authors = book_data['authors'][0]
 
         page = urllib2.urlopen('http://www.indiabookstore.net/isbn/' + str(isbn))
         soup = BS(page)
 
+        image = book_data['imageLinks']['thumbnail']
+        rating = book_data['averageRating']
+        category = "Uncategorized"
+
         summary = soup.findAll('em')[0].string
+        print name
+        print authors
         print summary
+        print image
+        print rating
+        print category
     except:
         pass
 
 if __name__ == '__main__':
-    #get_data(9788131710265)
-    google_books(9788131710265)
+    get_data(9788131710265)
+    #google_books(9788131710265)
