614f52132b5c8dc759b8f17b13f63a51136a01c1
Shubham Dokania <skd.1810@gmail.com>
2015-04-04 20:27:16 +0530

    Refining API

--- bookd/views.py
+++ bookd/views.py
@@ -6,22 +6,42 @@ from bookd.models import Book
 
 def apiCall(request):
     val = request.GET.get('isbn')
-    try:
-        response_value = google_books(val)
-    except:
+
+
+    bookVals = Book.Query.filter(isbn=str(val))
+
+    print len(bookVals), "SHUBHAM"
+    if len(bookVals) is not 0:
+        response = {}
+        for existing in bookVals:
+            response['publisher'] = existing.publisher
+            response['isbn'] = existing.isbn
+            response['title'] = existing.title
+            response['authors'] = existing.authors
+            response['summary'] = existing.summary
+            response['image'] = existing.image
+            response['rating'] = existing.rating
+            response['category'] = existing.category
+
+        print response
+
+        return HttpResponse(json.dumps(response), content_type="application/json")
+
+    else:
+        print "YAY"
         try:
-            response_value = get_data(val)
+            response_value = google_books(val)
         except:
-            response_value = {"title": 'Error'}
+            try:
+                response_value = get_data(val)
+            except:
+                response_value = {"title": 'Error'}
 
-    try:
         saveInParse(response_value)
-    except:
-        pass
 
-    return HttpResponse(json.dumps(response_value), content_type="application/json")
+        return HttpResponse(json.dumps(response_value), content_type="application/json")
 
 
 def saveInParse(response):
-    book_data = Book(isbn=response['isbn'], title=response['title'], publisher=response['publisher'], authors=response['authors'], rating=response['rating'], category=response['category'], image=response['image'], summary=response['summary'])
+    book_data = Book(isbn=response['isbn'], title=response['title'], publisher=response['publisher'], authors=response['authors'], rating=float(str(response['rating'])), category=response['category'], image=response['image'], summary=response['summary'])
     book_data.save()
